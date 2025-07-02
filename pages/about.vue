aa374056dcd6ffa543faa132fc6561bd743dd017
Lucas Pereira <lucasferrari4@gmail.com>
2025-07-02 09:43:36 -0600

    challenge 1

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="container">My name is Luke</h1>
+</template>
+
+<script>
+    export default {
+        name: 'AboutPage'
+    }
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        margin-top: 50px;
+        font-size: 24px;
+        color: #333;
+    }
+</style>
---
+++ pages/gallery.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Gallery Page</h1>
+        <p>Here's my gallery</p>
+    </div>
+</template>
